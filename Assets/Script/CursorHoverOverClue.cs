9e2eb14bc7d686a24a2b3bcc1a026db0fdf644f2
KrSiri-gp <sirikancharana08@gmail.com>
2025-06-09 15:53:00 +0530

    CursorChange

---
+++ Assets/Script/CursorHoverOverClue.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class CursorHoverOverClue : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine(CheckHover());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        CursorManager.Instance.SetNormalCursor();
+        StopAllCoroutines();
+    }
+
+    private System.Collections.IEnumerator CheckHover()
+    {
+        while (true)
+        {
+            int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, null);
+            if (linkIndex != -1)
+                CursorManager.Instance.SetClueCursor();
+            else
+                CursorManager.Instance.SetNormalCursor();
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Script/CursorManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public static CursorManager Instance;
+
+    public Texture2D normalCursor;
+    public Texture2D clueCursor;
+    public Vector2 hotSpot = Vector2.zero;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        SetNormalCursor();
+    }
+
+    public void SetNormalCursor()
+    {
+        Cursor.SetCursor(normalCursor, hotSpot, CursorMode.Auto);
+    }
+
+    public void SetClueCursor()
+    {
+        Cursor.SetCursor(clueCursor, hotSpot, CursorMode.Auto);
+    }
+}
