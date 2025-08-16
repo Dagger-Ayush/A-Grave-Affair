3d28b751942b4b61f11b873fc3a29a6dcef84af7
KrSiri-gp <sirikancharana08@gmail.com>
2025-08-16 18:56:37 +0530

    green
    clue color changes to green when it hovers on it

--- Assets/Script/CursorHoverOverClue.cs
+++ Assets/Script/CursorHoverOverClue.cs
@@ -4,30 +4,77 @@ using TMPro;
 
 public class CursorHoverOverClue : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private TextMeshProUGUI text;
+    [Header("Color Settings")]
+    public Color hoverColor = Color.green;
+
+    private TextMeshProUGUI textComponent;
     private Camera uiCamera;
     private Coroutine hoverRoutine;
     private bool isHovering = false;
-    private string currentHoveredClueId = "";
+    private int currentLinkIndex = -1;
+    private string currentClueId = "";
 
-    private void Awake()
+    void Awake()
     {
-        text = GetComponent<TextMeshProUGUI>();
+        textComponent = GetComponent<TextMeshProUGUI>();
         uiCamera = GetCanvasCamera();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (!isHovering && hoverRoutine == null)
+        if (!isHovering)
         {
             isHovering = true;
-            hoverRoutine = StartCoroutine(CheckHover());
+            hoverRoutine = StartCoroutine(HoverCheck());
         }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         StopHover();
+    }
+
+    private System.Collections.IEnumerator HoverCheck()
+    {
+        while (isHovering)
+        {
+            int newLinkIndex = TMP_TextUtilities.FindIntersectingLink(textComponent, Input.mousePosition, uiCamera);
+
+            // Reset previous link if index changed
+            if (currentLinkIndex != -1 && currentLinkIndex != newLinkIndex)
+            {
+                ResetLinkColor(currentLinkIndex);
+            }
+
+            currentLinkIndex = newLinkIndex;
+
+            if (currentLinkIndex != -1)
+            {
+                var linkInfo = textComponent.textInfo.linkInfo[currentLinkIndex];
+                currentClueId = linkInfo.GetLinkID();
+
+                if (!ClueManager.Instance.ClueCheck(currentClueId))
+                {
+                    SetLinkColor(currentLinkIndex, hoverColor);
+                    SetCursorState(CursorState.Clue);
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ClueManager.Instance.AddClue(currentClueId);
+                    }
+                }
+                else
+                {
+                    SetCursorState(CursorState.Normal);
+                }
+            }
+            else
+            {
+                SetCursorState(CursorState.Normal);
+            }
+
+            yield return null;
+        }
     }
 
     private void StopHover()
@@ -38,79 +85,55 @@ public class CursorHoverOverClue : MonoBehaviour, IPointerEnterHandler, IPointer
             hoverRoutine = null;
         }
 
-        if (isHovering)
+        if (currentLinkIndex != -1)
         {
-            ResetCursorState();
-            isHovering = false;
-            currentHoveredClueId = "";
+            ResetLinkColor(currentLinkIndex);
         }
+
+        isHovering = false;
+        currentLinkIndex = -1;
+        currentClueId = "";
+        SetCursorState(CursorState.Normal);
     }
 
-    private void ResetCursorState()
+    private void SetLinkColor(int linkIndex, Color color)
+    {
+        if (linkIndex < 0 || linkIndex >= textComponent.textInfo.linkCount) return;
+
+        var linkInfo = textComponent.textInfo.linkInfo[linkIndex];
+        for (int i = 0; i < linkInfo.linkTextLength; i++)
+        {
+            int charIndex = linkInfo.linkTextfirstCharacterIndex + i;
+            if (charIndex >= textComponent.textInfo.characterCount) continue;
+
+            var charInfo = textComponent.textInfo.characterInfo[charIndex];
+            if (!charInfo.isVisible) continue;
+
+            int meshIndex = charInfo.materialReferenceIndex;
+            int vertexIndex = charInfo.vertexIndex;
+
+            Color32[] vertexColors = textComponent.textInfo.meshInfo[meshIndex].colors32;
+            vertexColors[vertexIndex + 0] = color;
+            vertexColors[vertexIndex + 1] = color;
+            vertexColors[vertexIndex + 2] = color;
+            vertexColors[vertexIndex + 3] = color;
+        }
+        textComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+    }
+
+    private void ResetLinkColor(int linkIndex)
+    {
+        textComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
+    }
+
+    private void SetCursorState(CursorState state)
     {
         if (TabletManager.isTabletOpen)
             CursorManager.Instance.SetCursor(CursorState.Tablet);
         else
-            CursorManager.Instance.SetCursor(CursorState.Normal);
-        ObjectHovering.isRunning = false;
-    }
+            CursorManager.Instance.SetCursor(state);
 
-    private System.Collections.IEnumerator CheckHover()
-    {
-        while (isHovering)
-        {
-            int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, uiCamera);
-
-            // Reset if not hovering over any link
-            if (linkIndex == -1)
-            {
-                if (!string.IsNullOrEmpty(currentHoveredClueId))
-                {
-                    currentHoveredClueId = "";
-                    ResetCursorState();
-                }
-                yield return null;
-                continue;
-            }
-
-            // Get current hovered clue info
-            var linkInfo = text.textInfo.linkInfo[linkIndex];
-            currentHoveredClueId = linkInfo.GetLinkID();
-            bool isClueCollected = ClueManager.Instance.ClueCheck(currentHoveredClueId);
-
-            // Handle click only on uncollected clues
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (!isClueCollected)
-                {
-                    ClueManager.Instance.AddClue(currentHoveredClueId);
-                    ResetCursorState();
-                    yield return new WaitForEndOfFrame(); //  immediate cursor reset
-                    continue;
-                }
-            }
-
-            // Only show clue cursor for uncollected clues
-            if (!isClueCollected)
-            {
-                if (!ObjectHovering.isRunning)
-                {
-                    if (TabletManager.isTabletOpen)
-                        CursorManager.Instance.SetCursor(CursorState.Tablet); 
-                    else
-                        CursorManager.Instance.SetCursor(CursorState.Clue);
-                    ObjectHovering.isRunning = true;
-                }
-            }
-            else if (ObjectHovering.isRunning)
-            {
-                ResetCursorState();
-            }
-
-            yield return null;
-        }
-
-        StopHover();
+        ObjectHovering.isRunning = (state == CursorState.Clue);
     }
 
     private Camera GetCanvasCamera()
